244be88bba8cfa6d819b74a80647c876934a4762
qiuzhanghua <qiuzhanghua@icloud.com>
2016-03-28 10:56:09 +0800

    Use Camel 2.17.0 to simplify the project structure.
    And use Java 8 lambda.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # spring-boot-camel
 Spring Boot with Camel Demo
 
-Use Spring Boot 1.3.x and Apache Camel 2.16.2.
+Use Spring Boot 1.3.x and Apache Camel 2.17.0.
 
--- src/main/java/com/example/SpringBootCamelApplication.java
+++ src/main/java/com/example/SpringBootCamelApplication.java
@@ -24,203 +24,27 @@ public class SpringBootCamelApplication {
   @Autowired
   CamelContext camelContext;
 
+  @Autowired
+  private ProducerTemplate producerTemplate;
+
+
   @Bean
   public RouteBuilder route() {
     return new RouteBuilder() {
       @Override
       public void configure() throws Exception {
-        from("direct:start").to("log:end?level=INFO");
-        from("timer://foo?period=1000").process(new Processor() {
-          @Override
-          public void process(Exchange exchange) throws Exception {
-            System.out.println("some");
-            exchange.setOut(new Message() {
-              @Override
-              public String getMessageId() {
-                return null;
-              }
-
-              @Override
-              public void setMessageId(String messageId) {
-
-              }
-
-              @Override
-              public Exchange getExchange() {
-                return null;
-              }
-
-              @Override
-              public boolean isFault() {
-                return false;
-              }
-
-              @Override
-              public void setFault(boolean fault) {
-
-              }
-
-              @Override
-              public Object getHeader(String name) {
-                return null;
-              }
-
-              @Override
-              public Object getHeader(String name, Object defaultValue) {
-                return null;
-              }
-
-              @Override
-              public <T> T getHeader(String name, Class<T> type) {
-                return null;
-              }
-
-              @Override
-              public <T> T getHeader(String name, Object defaultValue, Class<T> type) {
-                return null;
-              }
-
-              @Override
-              public void setHeader(String name, Object value) {
-
-              }
-
-              @Override
-              public Object removeHeader(String name) {
-                return null;
-              }
-
-              @Override
-              public boolean removeHeaders(String pattern) {
-                return false;
-              }
-
-              @Override
-              public boolean removeHeaders(String pattern, String... excludePatterns) {
-                return false;
-              }
-
-              @Override
-              public Map<String, Object> getHeaders() {
-                return null;
-              }
-
-              @Override
-              public void setHeaders(Map<String, Object> headers) {
-
-              }
-
-              @Override
-              public boolean hasHeaders() {
-                return false;
-              }
-
-              @Override
-              public Object getBody() {
-                return "Some Body";
-              }
-
-              @Override
-              public Object getMandatoryBody() throws InvalidPayloadException {
-                return null;
-              }
-
-              @Override
-              public <T> T getBody(Class<T> type) {
-                return null;
-              }
-
-              @Override
-              public <T> T getMandatoryBody(Class<T> type) throws InvalidPayloadException {
-                return null;
-              }
-
-              @Override
-              public void setBody(Object body) {
-
-              }
-
-              @Override
-              public <T> void setBody(Object body, Class<T> type) {
-
-              }
-
-              @Override
-              public Message copy() {
-                return null;
-              }
-
-              @Override
-              public void copyFrom(Message message) {
-
-              }
-
-              @Override
-              public void copyAttachments(Message message) {
-
-              }
-
-              @Override
-              public DataHandler getAttachment(String id) {
-                return null;
-              }
-
-              @Override
-              public Set<String> getAttachmentNames() {
-                return null;
-              }
-
-              @Override
-              public void removeAttachment(String id) {
-
-              }
-
-              @Override
-              public void addAttachment(String id, DataHandler content) {
-
-              }
-
-              @Override
-              public Map<String, DataHandler> getAttachments() {
-                return null;
-              }
-
-              @Override
-              public void setAttachments(Map<String, DataHandler> attachments) {
-
-              }
-
-              @Override
-              public boolean hasAttachments() {
-                return false;
-              }
-
-              @Override
-              public String createExchangeId() {
-                return null;
-              }
-            });
-          }
+//        from("direct:start").to("log:end?level=INFO");
+        from("timer://foo?period=1000").process(exchange -> {
+          exchange.getIn().setBody("Hello, world with Java!");
         }).to("log:end?level=INFO");
       }
     };
   }
 
-  @Bean
-  public CommandLineRunner runner() {
-    return new CommandLineRunner() {
-      @Override
-      public void run(String... args) throws Exception {
-        camelContext.createProducerTemplate().sendBody("direct:start", "Hello, world");
-      }
-    };
-  }
-
-
   public static void main(String[] args) {
     final ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringBootCamelApplication.class, args);
     CamelSpringBootApplicationController applicationController =
         applicationContext.getBean(CamelSpringBootApplicationController.class);
-    applicationController.blockMainThread();
+    applicationController.run();
   }
 }
